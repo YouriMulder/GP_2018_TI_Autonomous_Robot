78d143b7a698019b97d09badad7082a49dae951f
youri <youri@demuldertjes.nl>
2018-03-27 12:40:49 +0200

    Light sensor init

---
+++ lightSensor.cpp
@@ -0,0 +1,31 @@
+#include "BrickPi3.h"
+#include <iostream>
+#include <singal.h>
+using namespace std;
+
+BrickPi BP;
+
+void exit_signal_handler(int signo);
+
+int main() {
+  signal(SIGINT, exit_signal_handler);
+
+  BP.detect();
+
+  BP.set_sensor_type(PORT_1, SENSOR_TYPE_NXT_LIGHT_ON);
+
+  sensor_light_t light;
+
+  while(true) {
+    if(BP.get_sensor(PORT_1, light) == 0) {
+      cout << "Light sensor (S0) : reflected" << setw(4) << Light.reflected << endl;
+    }
+  }
+}
+
+void exit_signal_handler(int signo) {
+  if(signo == SIGINT) {
+    BP.reset_all();
+    exit(-2);
+  }
+}
