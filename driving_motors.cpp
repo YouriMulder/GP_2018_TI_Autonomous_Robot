0974fd9dcd4a51d0f4e50f5c3e518faee006199f
gerritvanos <gerrit.vanos@student.hu.nl>
2018-03-28 17:17:27 +0200

    BP aangepast

--- driving_motors.cpp
+++ driving_motors.cpp
@@ -1,6 +1,6 @@
 #include "driving_motors.hpp"
 #include "BrickPi3.h"
-BrickPi3 BP;
+BrickPi3 BP_wheels;
 #include <iostream>
 using namespace std;
 
@@ -8,36 +8,36 @@ uint8_t L = PORT_A;
 uint8_t R = PORT_B;
 
 void initialize(){
-	BP.detect();
+	BP_wheels.detect();
 }
 
 void straight (float speed, char f_r) {
 	speed = speed/5;
 	speed = speed *32.767;
 	if (f_r == 'f') {
-		BP.set_motor_dps(L,speed);
-		BP.set_motor_dps(R,speed);
+		BP_wheels.set_motor_dps(L,speed);
+		BP_wheels.set_motor_dps(R,speed);
 	}
 	else if (f_r == 'r'){
-		BP.set_motor_dps(L,speed*-1);
-		BP.set_motor_dps(R,speed*-1);
+		BP_wheels.set_motor_dps(L,speed*-1);
+		BP_wheels.set_motor_dps(R,speed*-1);
 	}
 }
 
 void stop () {
-	BP.set_motor_power(L,0);
-	BP.set_motor_power(R,0);
+	BP_wheels.set_motor_power(L,0);
+	BP_wheels.set_motor_power(R,0);
 }
 
 void free () {
-	BP.set_motor_power(L,-128);
-	BP.set_motor_power(R,-128);
+	BP_wheels.set_motor_power(L,-128);
+	BP_wheels.set_motor_power(R,-128);
 }
 
 void turn (float speed, char f_r, float corner){
 	float left_motor;
 	float right_motor;
-	corner = 10*corner /100;
+	corner = 10*corner /180;
 	speed = speed /5;
 	
 	if (f_r == 'f'){
@@ -68,8 +68,8 @@ void turn (float speed, char f_r, float corner){
 		}
 	}
 	
-	BP.set_motor_dps(L,left_motor);
-	BP.set_motor_dps(R,right_motor);
+	BP_wheels.set_motor_dps(L,left_motor);
+	BP_wheels.set_motor_dps(R,right_motor);
 }
 
 void reset_motors(){
