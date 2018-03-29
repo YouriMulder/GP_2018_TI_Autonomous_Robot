1e3328ec0bede30d1419827e9216d80710da5176
bartbal <Pioneer12>
2018-03-29 11:44:26 +0200

    added color_get_reflection function

--- color_sensor.cpp
+++ color_sensor.cpp
@@ -10,6 +10,19 @@ int set_color_sensor(const uint8_t & port = PORT_2){
     return BP_color.set_sensor_type(port, SENSOR_TYPE_NXT_COLOR_FULL);
 }
 
+//retrun value between 0 to 100. 0 is black, 100 is white.
+int color_get_reflection(sensor_color_t data, uint8_t port = PORT_2){
+    BP.get_sensor(port, data);
+    float reflection = data.reflected_green;
+    reflection += data.reflected_red;
+    reflection += data.reflected_blue;
+    reflection = ((reflection / 3) - 320)/430 * 100;
+    if (reflection < 0){
+        return 0;
+    }
+    return (int) reflection;
+}
+
 //get the color sensor data
 int read_color_sensor(sensor_color_t data, uint8_t port = PORT_2){
     BP_color.get_sensor(port, data);
