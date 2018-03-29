ada4448bb87a3de8be1af6339608894ca372789d
Lars <lars.versteeg1999@gmail.com>
2018-03-29 11:58:30 +0200

    Touch_sensor
    Functies voor de touchsensor.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "BrickPi3.h"
+#include <iostream>
+#include "touch.hpp"
+using namespace std;
+
+BrickPi3 BP;
+
+int main(int argc, char **argv)
+{
+	void init_touch();
+	for(;;){
+		int returndata();
+	}
+}
---
+++ touch.hpp
@@ -0,0 +1,7 @@
+#ifndef _TOUCH_SOURCE_HPP
+#define _TOUCH_SOURCE_HPP
+
+void init_touch();
+int return_data();
+
+#endif /* _TOUCH_SOURCE_HPP */
