5b7b5d87898f1f29bc3d721458a96ac18e92b73c
bartbal <Pioneer12>
2018-03-29 11:50:21 +0200

    added color_get_reflection function to hpp file

--- color_sensor.hpp
+++ color_sensor.hpp
@@ -7,6 +7,9 @@ int set_color_sensor(const uint8_t & port = PORT_2);
 //get the color sensor data
 int read_color_sensor(sensor_color_t data, uint8_t port = PORT_2);
 
+//retrun value between 0 to 100. 0 is black, 100 is white.
+int color_get_reflection(sensor_color_t data, uint8_t port = PORT_2);
+
 //check if the sensor sees black
 bool color_detect_line(sensor_color_t data, uint8_t port = PORT_2);
 #endif //_COLOR_SENSOR_HPP
