f1278c60be57d9105eed260ca0604bf8460c20f1
bartbal <Pioneer12>
2018-04-03 14:19:14 +0200

    updated light_sensor.cpp

--- light_sensor.cpp
+++ light_sensor.cpp
@@ -4,26 +4,32 @@
 #include <unistd.h>
 using namespace std;
 
+//define BrickPi3 library
 BrickPi3 BP_light;
 
+//define light_data
 sensor_light_t light_data;
 
+//define default port for light sensor
+uint8_t default_light_port = PORT_1;
+
+//define calibration for light sensor
 calibrate_light light = {1480, 2430};
 
-//setup a light sensor. defauld PORT_1 is PORT_1
-int set_light_sensor(){
-    return BP_light.set_sensor_type(PORT_1, SENSOR_TYPE_NXT_LIGHT_ON);
+//setup a light sensor
+int set_light_sensor(uint8_t port = default_light_port){
+    return BP_light.set_sensor_type(port, SENSOR_TYPE_NXT_LIGHT_ON);
 }
 
 //get light sensor light_data.
-int read_light_sensor(){
-    BP_light.get_sensor(PORT_1, light_data);
+int read_light_sensor(uint8_t port = default_light_port){
+    BP_light.get_sensor(port, light_data);
     return light_data.reflected;
 }
 
 //check if the sensor sees black
-bool light_detect_line(){
-    int result = read_light_sensor();
+bool light_detect_line(uint8_t port = default_light_port){
+    int result = read_light_sensor(port);
     if(result > 2100){
         return true;
     }
@@ -31,10 +37,9 @@ bool light_detect_line(){
 }
 
 //return value between 0 and 100. 100 is white, 0 is black
-int light_get_reflection(){
-    BP_light.get_sensor(PORT_1, light_data);
+int light_get_reflection(uint8_t port = default_light_port){
+    BP_light.get_sensor(port, light_data);
     float reflection = light_data.reflected;
-    cout << "test1: " << reflection << "\n";
     reflection = (reflection - light.min)/(light.max - light.min) * 100;
     if(reflection < 0){
         reflection = 0;
@@ -46,14 +51,14 @@ int light_get_reflection(){
 }
 
 // reset min and max value of calibrate light
-void light_calibrate(){
+void light_calibrate(uint8_t port = default_light_port){
     float max = 0;
     float min = 0;
     char temp;
     cout << "put sensor on white\n";
     cin >> temp;
     for (int i = 0; i < 10; ++i) {
-        BP_light.get_sensor(PORT_1, light_data);
+        BP_light.get_sensor(port, light_data);
         if(light_data.reflected < min || min == 0){
             min = light_data.reflected;
         }
@@ -62,7 +67,7 @@ void light_calibrate(){
     cout << "put sensor on black\n";
     cin >> temp;
     for (int j = 0; j < 10; ++j) {
-        BP_light.get_sensor(PORT_1, light_data);
+        BP_light.get_sensor(port, light_data);
         if(light_data.reflected > max || max == 0){
             max = light_data.reflected;
         }
