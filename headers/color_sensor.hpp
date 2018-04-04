6b0d6e250835b433c53e663811290c90d380e7ca
youri <youri@demuldertjes.nl>
2018-04-04 14:25:14 +0200

    Header issue

--- headers/color_sensor.hpp
+++ headers/color_sensor.hpp
@@ -5,38 +5,29 @@
 
 using namespace std;
 
+#include <string>
+
 struct calibrate_color {
     int max;
     int min;
 };
 
-/**
- * @brief setup a color sensor, default port is PORT_2
- * @return error code
- */
+//read calibration from calibration_save file and save data to calibration struct
+void color_read_and_set_calibration_save();
+
+//setup a color sensor. defauld port is PORT_2
 int set_color_sensor();
 
-/**
- * @brief get reflected color value from sensor
- * @return return value between 0 to 100. 0 is black, 100 is white
- */
+//retrun value between 0 to 100. 0 is black, 100 is white.
 int color_get_reflection();
 
-/**
- * @brief get the color sensor data
- * @return sensor data
- */
+//get the color sensor data
 int read_color_sensor();
 
-/**
- * @brief check if the sensor detects black
- * @return boolean true for black false for white e.g.
- */
+//check if the sensor sees black
 bool color_detect_line();
 
-/**
- * @brief reset min and max value for calibrate color
- */
+// reset min and max value for calibrate color
 void color_calibrate();
 
 #endif //_COLOR_SENSOR_HPP
