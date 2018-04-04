05fcebbe1f4e05d142868cd90b89b85c9f8ccd97
bartbal <Pioneer12>
2018-04-04 11:37:40 +0200

    created auto calibration that saves to a file

---
+++ headers/calibrate.hpp
@@ -0,0 +1,8 @@
+#ifndef GP_2018_TI_AUTONOMOUS_ROBOT_CALIBRATE_HPP
+#define GP_2018_TI_AUTONOMOUS_ROBOT_CALIBRATE_HPP
+
+bool save_calibration(int max_color, int min_color, int max_light, int min_light);
+
+void calibrate_line_sensors();
+
+#endif //GP_2018_TI_AUTONOMOUS_ROBOT_CALIBRATE_HPP
