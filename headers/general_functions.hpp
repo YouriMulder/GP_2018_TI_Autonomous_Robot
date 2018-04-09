a15ea8862b305350db46361bc70b29c9c70c4183
youri <youri@demuldertjes.nl>
2018-04-09 13:50:54 +0200

    Bug fixes

--- headers/general_functions.hpp
+++ headers/general_functions.hpp
@@ -2,12 +2,13 @@
 #define GP_2018_TI_AUTONOMOUS_ROBOT_GENERAL_FUNCTIONS_HPP
 
 #include <vector>
+#include <string>
 
 // checks if a substring is the given string
-bool find_sub_string(string str, string sub);
+bool find_sub_string(std::string str, std::string sub);
 
 // removes substring from the given string
-string remove_sub_str(string str, string sub);
+std::string remove_sub_str(std::string str, std::string sub);
 
 // calculates the average value of the vector
 int average_vector(const std::vector<int>& v);
